bf2411aa4fb53c75fa6122d2cf27d0589bf0195f
Ariane <pikabooariane@hotmail.fr>
2021-02-11 17:40:16 +0200

    Mise en forme

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,23 +1,25 @@
 class SessionsController < ApplicationController
-    def new
-    end
-    def create
-        if !User.find_by(email:params[:email]).nil?
-            user = User.find_by(email:params[:email])
-            if user.authenticate(params[:password])
-                session[:user_id] = user.id
-                redirect_to root_path
-            else
-                flash[:danger] = "Mot de passe non valide"
-                render :new
-            end
-        else
-            flash[:danger] = "Email non valide"
-            render :new
-        end  
-    end
-    def destroy
-        session.delete(:user_id)
+  def new
+  end
+
+  def create
+    if !User.find_by(email:params[:email]).nil?
+      user = User.find_by(email:params[:email])
+      if user.authenticate(params[:password])
+        session[:user_id] = user.id
         redirect_to root_path
-    end
+      else
+        flash[:danger] = "Mot de passe non valide"
+        render :new
+      end
+    else
+      flash[:danger] = "Email non valide"
+      render :new
+    end  
+  end
+  
+  def destroy
+    session.delete(:user_id)
+    redirect_to root_path
+  end
 end
